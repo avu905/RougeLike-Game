ddc295970456db9978a50c86061dd84f96e93684
Alex Vu <Alex@Alexs-MacBook-Pro-2.local>
2020-05-10 18:53:51 -0700

    first commit - only skeleton code

---
+++ Game.h
@@ -0,0 +1,22 @@
+//
+//  Game.h
+//  CS 32 - Project 3
+//
+//  Created by Alex Vu on 5/10/20.
+//  Copyright © 2020 Alex Vu. All rights reserved.
+
+#ifndef GAME_INCLUDED
+#define GAME_INCLUDED
+
+// You may add data members and other member functions to this class.
+
+class Game
+{
+public:
+    Game(int goblinSmellDistance);
+    void play();
+private:
+};
+
+#endif // GAME_INCLUDED
+
---
+++ utilities.h
@@ -0,0 +1,28 @@
+//
+//  utilities.hpp
+//  CS 32 - Project 3
+//
+//  Created by Alex Vu on 5/10/20.
+//  Copyright © 2020 Alex Vu. All rights reserved.
+//
+
+// utilities.h
+
+// Note:  YOU MUST NOT MAKE ANY CHANGE TO THIS FILE!
+
+#ifndef UTILITIES_INCLUDED
+#define UTILITIES_INCLUDED
+
+const char ARROW_LEFT  = 'h';
+const char ARROW_RIGHT = 'l';
+const char ARROW_UP    = 'k';
+const char ARROW_DOWN  = 'j';
+
+char getCharacter();
+void clearScreen();
+int randInt(int lowest, int highest);  // random int from lowest to highest
+int randInt(int limit);                // random int from 0 to limit-1
+bool trueWithProbability(double p);    // return true with probability p
+
+#endif // UTILITIES_INCLUDED
+
